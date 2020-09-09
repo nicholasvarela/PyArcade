b56954abbc5f8ade970c2f43344d77bc8d0c9925
Nicholas Varela <nicholasjv@icloud.com>
2020-09-09 14:23:47 -0500

    Added Front Menu

--- snake.py
+++ snake.py
@@ -1,79 +1,120 @@
-#attributions PythonEngineer https://www.youtube.com/watch?v=M_npdRYD4K0&ab_channel=PythonEngineer
+# attributions PythonEngineer https://www.youtube.com/watch?v=M_npdRYD4K0&ab_channel=PythonEngineer
 import curses
 from random import randint
+import time
 
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
+# Import the necessary packages
+from consolemenu import ConsoleMenu, SelectionMenu
+from consolemenu.items import MenuItem, SelectionItem, SubmenuItem,CommandItem, FunctionItem
+def playSnake():
+    curses.initscr()
+    win = curses.newwin(20, 60, 0, 0)
+    win.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    win.border(0)
+    win.nodelay(1)
 
-#game
-snake = [(4,15), (4,9), (4,8)]
-food = (10,20)
+    # game
+    snake = [(4, 15), (4, 9), (4, 8)]
+    food = (10, 20)
 
-win.addch(food[0], food[1], '#')
+    win.addch(food[0], food[1], '#')
 
-ESC = 27
-key = curses.KEY_RIGHT
+    ESC = 27
+    key = curses.KEY_RIGHT
 
-score = 0
+    score = 0
 
-while key != ESC:
+    while key != ESC:
 
-    win.addstr(0,2,'Score ' + str(score) + ' ')
+        win.addstr(0, 2, 'Score ' + str(score) + ' ')
 
-    win.timeout(150-(len(snake)) // 5 + len(snake) //10 % 120) # increase speed based on length of the snake
+        win.timeout(150-(len(snake)) // 5 + len(snake) // 10 %
+                    120)  # increase speed based on length of the snake
 
-    prev_key = key
-    event = win.getch()
-    key = event if event != -1 else prev_key
+        prev_key = key
+        event = win.getch()
+        key = event if event != -1 else prev_key
 
-    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC] :
-        key = prev_key
+        if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+            key = prev_key
 
-    #calc next cords
-    y = snake[0][0]
-    x = snake[0][1]
-    if key == curses.KEY_DOWN:
-        y += 1
-    if key == curses.KEY_UP:
-        y -= 1
-    if key == curses.KEY_LEFT:
-        x -= 1
-    if key == curses.KEY_RIGHT:
-        x += 1
+    # calc next cords
+        y = snake[0][0]
+        x = snake[0][1]
+        if key == curses.KEY_DOWN:
+            y += 1
+        if key == curses.KEY_UP:
+            y -= 1
+        if key == curses.KEY_LEFT:
+            x -= 1
+        if key == curses.KEY_RIGHT:
+            x += 1
 
-    snake.insert(0, (y,x))
+        snake.insert(0, (y, x))
 
-    if y == 0: break
-    if y == 19: break
-    if x == 0: break
-    if x == 59: break
+        if y == 0:
+            break
+        if y == 19:
+           break
+        if x == 0:
+            break
+        if x == 59:
+           break
 
-    #check if snake it's self
-    if(snake[0] in snake[1:]): break
+    # check if snake it's self
+        if(snake[0] in snake[1:]):
+            break
 
-    if snake[0] == food:
-        score += 1
-        food = ()
-        while food == ():
-            food = (randint(1,18), randint(1,58))
-            if food in snake:
-                 food = ()
-        win.addch(food[0], food[1], '#')
-    else: 
-        last = snake.pop()
-        win.addch(last[0],last[1] , ' ')
-    win.addch(snake[0][0],snake[0][1] , ' ')
+        if snake[0] == food:
+            score += 1
+            food = ()
+            while food == ():
+                food = (randint(1, 18), randint(1, 58))
+                if food in snake:
+                    food = ()
+            win.addch(food[0], food[1], '#')
+        else:
+            last = snake.pop()
+            win.addch(last[0], last[1], ' ')
+        win.addch(snake[0][0], snake[0][1], ' ')
 
+        for c in snake:
+            win.addch(c[0], c[1], '*')
 
-    for c in snake :
-        win.addch(c[0], c[1], '*')
+    curses.endwin()
+    print("Final score is " + str(score))
+def playTwo():
+    curses.initscr()
+    win = curses.newwin(20, 60, 0, 0)
+    win.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    win.border(0)
+    win.nodelay(1)
+
+    # game
+    win.refresh()
+    time.sleep(5)
+    curses.endwin()
     
 
-curses.endwin()
-print("Final score is " + score) 
+def startMenu():
+    # Create the menu
+    menu = ConsoleMenu("Arcade", "Select a Game to Begin")
+    # A FunctionItem runs a Python function when selected
+    snake_item = FunctionItem("Play Snake", playSnake)
+    two_item = FunctionItem("Play Two", playTwo)
+    # A CommandItem runs a console command
+    
+    # Once we're done creating them, we just add the items to the menu
+    
+    menu.append_item(snake_item)
+    menu.append_item(two_item)
 
+
+    # Finally, we call show to show the menu and allow the user to interact
+    menu.show()
+
+startMenu()
